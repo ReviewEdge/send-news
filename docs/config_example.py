5fa39f4f768dbca0dbdc0c2e65a23763023971c0
Logan Richardson <reviewedge56@gmail.com>
2020-03-30 18:33:00 -0400

    Create config_example.py

---
+++ docs/config_example.py
@@ -0,0 +1,6 @@
+# When you create your own configuration file, make sure it is named "config.py"
+
+# send_news info:
+sender_email_address = "XXXXX"
+sender_email_password = "XXXXX"
+rec_email_address = "XXXXX"
