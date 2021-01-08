353759f1835bd44da0b0fe4b43b8887a5b0db55e
ReviewEdge <reviewedge56@gmail.com>
2021-01-08 13:26:22 -0500

    added google sheets email list to send_news

--- sample/send_news.py
+++ sample/send_news.py
@@ -2,8 +2,22 @@ import config
 from furtherpy.sample import date_conv_tool
 from email_reader_repo.sample import email_tool
 import get_news
+from spotify_controller_repo.sample import gsheets_tool
 
 # change send list to a Google sheet
+
+
+def get_email_list():
+    service = gsheets_tool.authenticate_sheets_api()
+    current_sheet_id = config.news_email_rec_sheets_id
+
+    raw_email_list = gsheets_tool.get_all_sheets_data(service, current_sheet_id, "A:A")
+
+    email_list = [val for sublist in raw_email_list for val in sublist]
+
+    return email_list
+
+
 
 
 def main():
@@ -17,9 +31,13 @@ def main():
 
     headlines_printable_decoded = str(headlines_printable_ascii.decode("ascii"))
 
-    # Sends email
-    email_tool.send_email(config.sender_email_address, config.sender_email_password,
-                         config.rec_email_address, subject, headlines_printable_decoded)
+    # Gets the wiki-a-day email list
+    mailing_list = get_email_list()
+
+    # Sends email to every address on list
+    for rec_address in mailing_list:
+        email_tool.send_email(config.sender_email_address, config.sender_email_password, rec_address,
+                              subject, headlines_printable_decoded)
 
     print("[send_news] Sent news update")
 
